6416c65f2258a7e58c529cc8cb31b0006e3d302a
Gennadiy <32727741+UN7FGO@users.noreply.github.com>
2019-03-07 15:30:39 +0600

    Update MFS_TEA5767_FM_RADIO.ino

--- MFS_TEA5767_FM_RADIO.ino
+++ MFS_TEA5767_FM_RADIO.ino
@@ -1,31 +1,45 @@
+// http://un7fgo.gengen.ru (C) 2019
+// https://github.com/UN7FGO 
+// 
+// Скетч FM-приемника для модулей Multi Function Shield и TEA5767.
+
+// подключаем библиотеку для Multi Function Shield
 #include <MFShield.h>
+// подключаем бибилиотеку для I2C интерфейса
 #include <Wire.h>
+// подключаем библиотеку для модуля TEA5767
 #include <TEA5767N.h>
 
-// button assignment settings
+// определяем назначение кнопок на Multi Function Shield
 #define BUTTON_START  2
 #define BUTTON_PLUS   3
 #define BUTTON_MINUS  1
 
+// создаем объекты для модулей 
 MFShield mfs;
 TEA5767N radio = TEA5767N();
 
+// создаем переменную и устанавливаем начальную частоту
+// частота хранится в целых значениях, а на модуль отпарвляем ее деленную на 10
 int freq = 1029;
 uint32_t t; 
 
-/* ============= BUTTONS HANDLER FUNCTION ============= */
+// Обрабатываем нажатия клавиш
 void keyAction (uint8_t key)
 {
   switch (key)
   {
+    // увеличиваем частоту на 0.1 МГц
     case BUTTON_PLUS:
       freq += 1;
       break;
 
+    // увеличиваем частоту на 0.1 МГц
     case BUTTON_MINUS:
       freq -= 1;
       break;
 
+    // отпарвляем текущее значение частоты в модуль ФМ-приемника
     case BUTTON_START:
       mfs.showDisplay (false);
       radio.setStandByOn();
@@ -34,18 +48,21 @@ void keyAction (uint8_t key)
       mfs.showDisplay (true);
       break;
   }
-  mfs.display(freq, true);     // update counter value on display
+  // выводим текущую частоту на индикатор
+  mfs.display(freq, true);     
 }
 /* =================================================== */
 void setup() {
-  //Turns the radio off
+  // выключаем радио, если оно было включено
   radio.setStandByOff();
-  // assign handler function for buttons (see above)
+  // привязываем нашу функцию обработки кнопок к объекту Multi Function Shield
   mfs.onKeyPress (keyAction);
+  // отправляем текущую частоту в модуль ФМ-радио
   radio.selectFrequency(float(freq) / 10);
 }
 
 void loop() {
-  mfs.display(freq, true);     // update counter value on display
+  // выводим текущую частоту на индикатор
+  mfs.display(freq, true);
   mfs.loop(); 
 }
